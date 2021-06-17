86bd0463014eb6c3bc5266cbc82431ad889a4bfd
Ibbathon <ibbathon@gmail.com>
2021-06-17 09:31:08 -0500

    Add some error-checking to the IPython helper

--- helpers/ipython_startup.py
+++ helpers/ipython_startup.py
@@ -1,4 +1,20 @@
-import os, IPython
-os.environ['PYTHONSTARTUP'] = ''  # Prevent running this again
-IPython.start_ipython()
-raise SystemExit
+import os
+
+try:
+    import IPython
+    ipython_available = True
+except ModuleNotFoundError:
+    ipython_available = False
+
+try:
+    __IPYTHON__
+    ipython_initialized = True
+except NameError:
+    ipython_initialized = False
+
+if ipython_available and not ipython_initialized:
+    os.environ['PYTHONSTARTUP'] = ''  # Prevent running this again
+    IPython.start_ipython()
+    raise SystemExit
+elif not ipython_available:
+    print('ipython not installed')
